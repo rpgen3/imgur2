0b4a661b0059e367a0eb11b45de3b354cb4330f8
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-07-14 22:59:20 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,14 +4,33 @@
     const rpgen3 = await importAll([
         'input',
         'strToImg',
-        'imgur'
+        'imgur',
+        'util'
     ].map(v => `https://rpgen3.github.io/mylib/export/${v}.mjs`));
-    const {addInputStr, imgur, bufToImg, imgToBuf} = rpgen3;
+    const {getTime, addInputStr, imgur, bufToImg, imgToBuf} = rpgen3;
     const body = $('body').css({
         'text-align': 'center',
         padding: '1em'
     });
     $('<h1>').appendTo(body).text('imgurを汎用アップローダーとして使う裏技');
+    $('<h2>').appendTo(body).text('1MB以上はimgurにアップロードすると劣化するのでデコードに失敗します');
+    const console = new class {
+        constructor(){
+            this.elm = $('<span>').appendTo($('<div>').appendTo(body));
+        }
+        log(...arg){
+            return this.elm.text(arg.join(' ') + `(${getTime()})`).css({
+                color: 'blue',
+                backgroundColor: 'lightblue'
+            });
+        }
+        error(...arg){
+            this.log(...arg).css({
+                color: 'red',
+                backgroundColor: 'pink'
+            });
+        }
+    };
     $('<input>').appendTo(body).attr({
         type: 'file'
     }).on('change', ({target}) => {
@@ -25,11 +44,9 @@
             copy: true,
             value: id
         });
-        addInputStr(output,{
-            label: 'deletehash',
-            copy: true,
-            value: `id=${id}&deletehash=${deletehash}&token=${token}`
-        });
+        imgur.delete({deletehash, token})
+            .then(() => console.log('削除しました'))
+            .catch(() => console.error('削除できませんでした'));
         output.append(await imgur.load(id));
     };
     const input = addInputStr(body,{
